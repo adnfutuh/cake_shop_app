e820d9419a5943c532ed6abb8e66bc0309a125d1
rizki <rizkiadnanfutuh@gmail.com>
2024-08-13 17:53:56 +0700

    feat : add bottomNavigationBar

---
+++ lib/home.dart
@@ -0,0 +1,25 @@
+import 'package:cake_shop_app/widgets/navbar_widget.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Adnan Bakery',
+          ),
+        ),
+      ),
+      bottomNavigationBar: const NavbarWidget(),
+    );
+  }
+}
